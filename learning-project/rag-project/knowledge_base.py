2b32530701e85a1fb8b8341d9ad3daff16d0afb5
Reed0124 <792936825@qq.com>
2026-05-21 12:23:01 +0800

    为知识库添加MD5去重功能

--- learning-project/rag-project/knowledge_base.py
+++ learning-project/rag-project/knowledge_base.py
@@ -1,27 +1,34 @@
 """
 知识库
 """
-
+import hashlib
 import os
+import config_data as config
 
 
-def check_md5():
-    """
-    检查传入的md5字符串是否已经被处理过了
-    """
-    pass
+def check_md5(md5_str: str):
+    """检查传入的md5字符串是否已有"""
+    if not os.path.exists(config.md5_path):  # 检查文件是否存在
+        open(config.md5_path, 'w', encoding='utf-8').close()  # w模式自动创建文件
+        return False
+    else:
+        with open(config.md5_path, 'r', encoding='utf-8') as f:
+            for line in f:
+                if line.strip() == md5_str:
+                    return True
+        return False
 
-def save_md5():
-    """
-    将传入的md5字符串，记录到文件中保存
-    """
-    pass
 
-def get_string_md5():
-    """
-    获取传入的字符串转换为md5字符串
-    """
-    pass
+def save_md5(md5_str: str):
+    """将传入的md5字符串保存到文件中"""
+    with open(config.md5_path, 'a', encoding='utf-8') as f:
+        f.write(md5_str + '\n')
+
+
+def get_string_md5(input_str: str, encoding='utf-8'):
+    """将传入的字符串转换为md5字符串"""
+    str_bytes = input_str.encode(encoding)
+    return hashlib.md5(str_bytes).hexdigest() # 返回md5的16进制字符串
 
 
 class KnowledgeBaseService(object):
@@ -37,4 +44,14 @@ class KnowledgeBaseService(object):
         pass
 
 
+if __name__ == '__main__':
+    # r = get_string_md5("我是楞啊")
+    # r2 = get_string_md5("我是人啊")
+    # r3 = get_string_md5("我是人啊")
+    # print(r, r2, r3)
+
+    # save_md5("0c39c82e86a5ac0ec6c9fef235d45eb5")
+    # print(check_md5("0c39c82e86a5ac0ec6c9fef235d45eb5"))
+
+    pass
 
