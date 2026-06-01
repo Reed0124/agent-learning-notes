27bb892c0ef43cbefca278c4c033803460a99634
Reed0124 <792936825@qq.com>
2026-06-02 00:33:59 +0800

    完成Agent项目的工具开发

--- learning-project/agent-project/utils/file_handler.py
+++ learning-project/agent-project/utils/file_handler.py
@@ -1,8 +1,11 @@
 import os, hashlib
 from logger_handler import logger
+from langchain_core.documents import Document
+from langchain_community.document_loaders import PyPDFLoader, TextLoader
+
 def get_file_md5_hex(file_path: str):
     """
-    获取文件的MD5的16进制字符串
+    获取文件的MD5的16进制字符串，用于文件去重
     :param file_path: 文件路径
     :return: MD5的16进制字符串
     """
@@ -20,7 +23,8 @@ def get_file_md5_hex(file_path: str):
 
     try:
         with open(file_path, "rb") as f: # 二进制打开文件
-            while chunk := f.read(chunk_size):
+            # 分片读取文件放入hashlib.md5()对象中，以免爆内存
+            while chunk := f.read(chunk_size): # 海象运算符（Walrus Operator) | 循环读取赋值
                 md5_obj.update(chunk)
             return md5_obj.hexdigest()
 
@@ -29,16 +33,38 @@ def get_file_md5_hex(file_path: str):
         return None
 
 
-def listdir_with_allowed_type():
+def listdir_with_allowed_type(path: str, allowed_types: tuple[str]):
     """
-    返回文件夹内的文件列表（允许的文件后缀）
+    返回文件夹内的文件列表
     :return:
     """
-    pass
+    files = []
 
-def pdf_loader():
-    pass
+    if not os.path.isdir(path):
+        logger.error(f"[listdir_with_allowed_type] {path} 不是一个文件夹")
+        return allowed_types
 
-def txt_loader():
-    pass
+    for file in os.listdir(path):
+        if file.endswith(allowed_types):
+            files.append(os.path.join(path, file))
+
+    return tuple[files]
+
+
+def pdf_loader(file_path: str, password=None) -> list[Document]:
+    """
+    加载PDF文件
+    :param file_path: 文件路径
+    :param password: PDF文件的密码
+    :return: list[Document]
+    """
+    return PyPDFLoader(file_path, password=password).load()
+
+def txt_loader(file_path: str) -> list[Document]:
+    """
+    加载txt文件
+    :param file_path: 文件路径
+    :return: list[Document]
+    """
+    return TextLoader(file_path).load()
 
