e3b50c6924065f4886c6eb49f23d906b61794f47
Reed0124 <792936825@qq.com>
2026-08-06 16:40:44 +0800

    测试Deepseek的web_research功能

--- LangChain/Agent/code/01_init_chat_model.py
+++ LangChain/Agent/code/01_init_chat_model.py
@@ -18,6 +18,7 @@ import os
 from langchain.chat_models import init_chat_model
 from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 from langchain_deepseek import ChatDeepSeek
+from rich import print as rprint
 
 # 读取配置文件中的.env文件 (后续初始化模型的时候可以不加api和url参数，自动检测)
 from dotenv import load_dotenv
@@ -26,43 +27,53 @@ load_dotenv(override=True)
 DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API_KEY")
 DEEPSEEK_BASE_URL = os.getenv("DEEPSEEK_BASE_URL")
 
-# 创建Deepseek的模型
-llm = ChatDeepSeek(
+model = ChatDeepSeek(
     model="deepseek-v4-flash",
     # api_key=DEEPSEEK_API_KEY,
     # api_base=DEEPSEEK_BASE_URL,
+    extra_body={
+        # "thinking": {"type": "disabled"},
+        "enable_search": True
+
+    }
+
 )
 
-# langchain 1.0 提供了统一初始化模型的方法 init_chat_model
-model = init_chat_model(
-    model="deepseek-v4-flash", # 模型名称
-    api_key=DEEPSEEK_API_KEY,
-    base_url=DEEPSEEK_BASE_URL,
-    temperature=0.7, # 模型温度，范围0-1，默认0.7
-)
+# model = init_chat_model(
+#     model="deepseek-v4-flash", # 模型名称
+#     api_key=DEEPSEEK_API_KEY,
+#     base_url=DEEPSEEK_BASE_URL,
+#     temperature=0.7, # 模型温度，范围0-1，默认0.7
+#
+#     # extra_body={
+#     #     # "thinking": {"type": "disabled"},
+#     #     "enable_search": True
+#     #
+#     # }
+# )
 
 # 调用模型
 # 1、入参字符串
-response1 = model.invoke("巴威台风对苏州的影响如何")
+# response1 = model.invoke("巴威台风对苏州的影响如何")
 
 # 2、字典列表 (多轮对话场景)
 messages = [
     {"role": "system", "content": "你是一个话少的助手"},
-    {"role": "user", "content": "巴威台风对苏州的影响如何"},
+    {"role": "user", "content": "2026年有两位华人获得了菲尔兹奖是谁"},
     # {"role": "assistant", "content": "好的，正在查找"}
 ]
 response2 = model.invoke(messages)
+rprint(response2)
 
 # 3、消息对象列表
-message_list = [
-    SystemMessage(content="你是一个话少的助手"),
-    HumanMessage(content="巴威台风对苏州的影响如何"),
-    # AIMessage(content="好的，正在查找")
-]
-response3 = model.invoke(message_list)
-print(response3.content)
-print(response3.content_blocks)
-# invoke的返回值
+# message_list = [
+#     SystemMessage(content="你是一个话少的助手"),
+#     HumanMessage(content="巴威台风对苏州的影响如何"),
+#     # AIMessage(content="好的，正在查找")
+# ]
+# response3 = model.invoke(message_list)
+# print(response3.content)
+# print(response3.content_blocks)
 
 
 
